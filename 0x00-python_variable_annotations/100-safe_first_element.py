5345d8a359b52dc57dd8d6520a7ba6d31ec55915
Kalule Joshua <kaljoshkalule@gmail.com>
2024-07-06 02:26:03 +0000

    add file 100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,6 +6,7 @@ from typing import Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Annotate sefe first element."""
     if lst:
         return lst[0]
     else:
