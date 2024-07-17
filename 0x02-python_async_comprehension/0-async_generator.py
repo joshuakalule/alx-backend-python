c9ba3a9ac3b9dde83b41663c9afbd2cbb96bd729
Kalule Joshua <kaljoshkalule@gmail.com>
2024-07-17 15:40:46 +0000

    update typing to Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Implement async generator."""
     for _ in range(10):
         yield random.uniform(0, 10)
