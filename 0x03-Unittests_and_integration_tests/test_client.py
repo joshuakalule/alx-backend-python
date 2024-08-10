2f7b7904514a45ad66b3a26022f4912f1e80e513
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-10 19:12:04 +0300

    add test for return_value=payload

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,11 +5,25 @@
 import client  # type: ignore
 from parameterized import parameterized  # type: ignore
 import unittest  # type: ignore
-from unittest.mock import patch  # type: ignore
+from unittest.mock import patch, PropertyMock  # type: ignore
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """Test the GithubOrgClientClass."""
+
+    @parameterized.expand([
+        ('google', {'repos_url': 'https://api.github.com/orgs/google/repos'})
+    ])
+    def test_public_repos_url(self, org, payload):
+        """Test the public_repos_url property."""
+        target = 'client.GithubOrgClient.org'
+        with patch(target, new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = payload
+
+            test_client = client.GithubOrgClient(org)
+
+            expected = payload['repos_url']
+            self.assertEqual(test_client._public_repos_url, expected)
 
     @parameterized.expand([
         ('google', {'login': 'google'}),
