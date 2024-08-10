d2b15fbeb6bd44f0aa3335a0de6484ea270ae7e5
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-11 01:23:52 +0300

    add test for publi_repos()

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -11,6 +11,25 @@ from unittest.mock import patch, PropertyMock  # type: ignore
 class TestGithubOrgClient(unittest.TestCase):
     """Test the GithubOrgClientClass."""
 
+    @patch(
+        'client.get_json',
+        new_callable=PropertyMock,
+        return_value=[{'name': 'repo1'}, {'name': 'repo2'}])
+    def test_public_repos(self, mock_get_json):
+        """Test GithubOrgClient.public_repos()"""
+        target = 'client.GithubOrgClient._public_repos_url'
+        with patch(target, new_callable=PropertyMock) as mock_property:
+            mock_property.return_value = 'doesnt matter'
+
+            test_client = client.GithubOrgClient("test")
+
+            test_public_repos = test_client.public_repos()
+
+            self.assertEqual(test_public_repos, ['repo1', 'repo2'])
+
+            mock_property.assert_called_once()
+            mock_get_json.assert_called_once()
+
     @parameterized.expand([
         ('google', {'repos_url': 'https://api.github.com/orgs/google/repos'})
     ])
