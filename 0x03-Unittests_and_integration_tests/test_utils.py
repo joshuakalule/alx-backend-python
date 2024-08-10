17d9bafaec135d90d454f6a11dee58d9d032ec24
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-10 06:19:47 +0300

    add test for get_json

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 """Task 0. Parametize a unit test."""
 
-import unittest  # type: ignore
+import unittest
+from unittest import mock
 from parameterized import parameterized  # type: ignore
-from utils import access_nested_map  # type: ignore
+import utils  # type: ignore
 from typing import Any  # type: ignore
+
+
+class TestGetJson(unittest.TestCase):
+    """ Test class for the get json function."""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload) -> None:
+        """Test that fetching test_url returns test_payload."""
+        with unittest.mock.patch('utils.requests.get') as mock_requests_get:
+            mock_response = mock.Mock()
+            mock_response.json.return_value = test_payload
+            mock_requests_get.return_value = mock_response
+
+            payload = utils.get_json(test_url)
+            mock_requests_get.assert_called_once_with(test_url)
+            self.assertEqual(payload, test_payload)
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -18,7 +38,7 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map: dict, path: tuple,
                                result: Any) -> None:
         """Test that nested_map, with path results into result."""
-        self.assertEqual(access_nested_map(nested_map, path), result)
+        self.assertEqual(utils.access_nested_map(nested_map, path), result)
 
     @parameterized.expand([
         ({}, ("a", )),
@@ -28,4 +48,4 @@ class TestAccessNestedMap(unittest.TestCase):
                                          path: tuple) -> None:
         """Test that nested_map called with path raises KeyError."""
         with self.assertRaises(KeyError):
-            access_nested_map(nested_map, path)
+            utils.access_nested_map(nested_map, path)
